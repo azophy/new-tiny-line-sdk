6672eb057b0e020e53deac19efe20ff10c803071
Abdurrahman Shofy Adianto <azophy@gmail.com>
2019-06-09 15:01:31 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+New tiny-line-sdk
+=================
+
+New working tiny-line-sdk which work with the latest LINE Messaging API. Adapted
+from code in https://github.com/HalimZamal/LineBotExample
+
+Try it on heroku using deploy button or copy content to a hook.io instance.
+
+Reference:
+- https://github.com/line/line-bot-sdk-php/tree/master/line-bot-sdk-tiny
+- http://gueprogramer.com/coding/create-line-chat-bot-and-messaging-api-use-php-simple-example/
+- https://medantechno.com/read/news/33/tutorial-membuat-bot-messaging-api-line-dengan-php-bagian-2
+- https://medantechno.com/read/news/23/tutorial-membuat-bot-messaging-api-line -> paling tua, kayaknya yang original
+- https://tempatilmukite.blogspot.com/2018/01/membuat-bot-line-dengan-hosting.html
+  - https://github.com/IrfanMaulana00/Line-Bot
---
+++ bot.php
@@ -0,0 +1,163 @@
+<?php 
+if (!function_exists('hash_equals')) 
+{
+    defined('USE_MB_STRING') or define('USE_MB_STRING', function_exists('mb_strlen'));
+
+    function hash_equals($knownString, $userString)
+    {
+        $strlen = function ($string) {
+            if (USE_MB_STRING) {
+                return mb_strlen($string, '8bit');
+            }
+
+            return strlen($string);
+        };
+
+        if (($length = $strlen($knownString)) !== $strlen($userString)) {
+            return false;
+        }
+
+        $diff = 0;
+
+        for ($i = 0; $i < $length; $i++) {
+            $diff |= ord($knownString[$i]) ^ ord($userString[$i]);
+        }
+        return $diff === 0;
+    }
+}
+
+class LINEBotTiny
+{
+    public function __construct($channelAccessToken, $channelSecret)
+    {
+        $this->channelAccessToken = $channelAccessToken;
+        $this->channelSecret = $channelSecret;
+    }
+
+    public function parseEvents()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405);
+            error_log("Method not allowed");
+            exit();
+        }
+
+        $entityBody = file_get_contents('php://input');
+
+        if (strlen($entityBody) === 0) {
+            http_response_code(400);
+            error_log("Missing request body");
+            exit();
+        }
+
+        if (!hash_equals($this->sign($entityBody), $_SERVER['HTTP_X_LINE_SIGNATURE'])) {
+            http_response_code(400);
+            error_log("Invalid signature value");
+            exit();
+        }
+
+        $data = json_decode($entityBody, true);
+        if (!isset($data['events'])) {
+            http_response_code(400);
+            error_log("Invalid request body: missing events property");
+            exit();
+        }
+        return $data['events'];
+    }
+
+    public function replyMessage($message)
+    {
+        $header = array(
+            "Content-Type: application/json",
+            'Authorization: Bearer ' . $this->channelAccessToken,
+        );
+
+        $context = stream_context_create(array(
+            "http" => array(
+                "method" => "POST",
+                "header" => implode("\r\n", $header),
+                "content" => json_encode($message),
+            ),
+        ));
+		$response = exec_url('https://api.line.me/v2/bot/message/reply',$this->channelAccessToken,json_encode($message));
+    }
+	
+    public function pushMessage($message) 
+    {
+        
+		$response = exec_url('https://api.line.me/v2/bot/message/push',$this->channelAccessToken,json_encode($message));
+    }
+	
+    public function profil($userId)
+    {
+		return json_decode(exec_url('https://api.line.me/v2/bot/profile/'.$userId,$this->channelAccessToken));
+    }
+
+    private function sign($body)
+    {
+        $hash = hash_hmac('sha256', $body, $this->channelSecret, true);
+        $signature = base64_encode($hash);
+        return $signature;
+    }
+}
+
+function exec_url($fullurl,$channelAccessToken,$message=Null)
+{
+		$header = array(
+            "Content-Type: application/json",
+            'Authorization: Bearer '.$channelAccessToken,
+        );
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        if ($message != Null) {
+            curl_setopt($ch, CURLOPT_POST,           1 );
+            curl_setopt($ch, CURLOPT_POSTFIELDS,     $message); 
+        }
+		curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($ch, CURLOPT_URL, $fullurl);
+		
+		$returned =  curl_exec($ch);
+	
+		return($returned);
+}
+
+// ======== MAIN PROGRAM SECTION ===========
+
+$channelAccessToken = getenv('LINE_API_ACCESS_TOKEN'); //sesuaikan 
+$channelSecret = getenv('LINE_API_SECRET');//sesuaikan
+//$channelAccessToken = $Hook['env']['LINE_API_ACCESS_TOKEN']; //sesuaikan 
+//$channelSecret = $Hook['env']['LINE_API_SECRET'];//sesuaikan
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage([
+                        'replyToken' => $event['replyToken'],
+                        'messages' => [
+                            [
+                                'type' => 'text',
+                                'text' => $message['text']
+                            ]
+                        ]
+                    ]);
+                    break;
+                default:
+                    error_log('Unsupported message type: ' . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log('Unsupported event type: ' . $event['type']);
+            break;
+    }
+};
